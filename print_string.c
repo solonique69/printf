ec02ddb76164fcadccfaecbac1cea419911ca41f
Solomon Okomowho <solonique2020@gmail.com>
2023-11-17 19:05:06 +0100

    made modifications

--- print_string.c
+++ print_string.c
@@ -9,6 +9,12 @@
 
 int print_string(char *str)
 {
+	 if (str == NULL)
+	 {
+		 write(1, "(null)", 6);
+		 return (6);  /*Return the length of "(null)"*/
+	 }
+
 	int len = 0;
 
 	while (str[len] != '\0')
