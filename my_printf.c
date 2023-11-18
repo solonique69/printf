a8939ff989625ba0e4070f49558c3fc9ca5bf133
Solomon Okomowho <solonique2020@gmail.com>
2023-11-18 19:30:49 +0100

    Modified print_string.c

--- my_printf.c
+++ my_printf.c
@@ -2,9 +2,8 @@
 
 /**
  * _printf - Custom printf function
- * @format: Format string containing conversion specifiers
- *
- * Return: Number of characters printed (excluding null byte)
+ * @format: Format string containing conversion specifier
+ * Return: Number of characters printed
  */
 
 int _printf(const char *format, ...)
@@ -14,7 +13,8 @@ int _printf(const char *format, ...)
 
 	if (format == NULL)
 	{
-		return (-1); /*handles NULL format string*/
+		write(1, "(null)", 6);
+		return (6); /* Return the length of "(null)"*/
 	}
 
 	va_start(args, format);
@@ -22,15 +22,26 @@ int _printf(const char *format, ...)
 	while (*format != '\0')
 	{
 		if (*format == '%' && (*(format + 1) == 'c' ||
-			*(format + 1) == 's' || *(format + 1) == '%'))
+					*(format + 1) == 's' || *(format + 1) == '%'))
 		{
-			/* Handle conversion specifiers 'c', 's', and '%'*/
-			count += handle_specifier(*(format + 1), args);
-			format += 2; /* Move to the next format specifier*/
+			switch (*(format + 1))
+			{
+				case 'c':
+					count += print_char(va_arg(args, int));
+					break;
+				case 's':
+					count += print_string(va_arg(args, char *));
+					break;
+				case '%':
+					count += print_percent();
+					break;
+			}
+
+			format += 2; /*Move to the next format specifier*/
 		}
+
 		else
 		{
-			/* Regular character, write to stdout*/
 			write(1, format, 1);
 			count++;
 			format++;
@@ -38,5 +49,6 @@ int _printf(const char *format, ...)
 	}
 
 	va_end(args);
+
 	return (count);
 }
