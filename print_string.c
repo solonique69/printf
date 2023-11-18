93df177e91fa3cd5ddcac1fef3abd94f12614bd4
Solomon Okomowho <solonique2020@gmail.com>
2023-11-18 13:39:33 +0100

    made modifications

--- print_string.c
+++ print_string.c
@@ -1,19 +1,10 @@
-#include "main.h"
-
-/**
- * print_string - Print a string to stdout
- * @str: The string to print
- *
- * Return: Number of characters printed
- */
-
 int print_string(char *str)
 {
-	 if (str == NULL)
-	 {
-		 write(1, "(null)", 6);
-		 return (6);  /*Return the length of "(null)"*/
-	 }
+	if (str == NULL)
+	{
+		write(1, "(null)", 6);
+		return (6);  /*Return the length of "(null)"*/
+	}
 
 	int len = 0;
 
